4962b9835f269671dc82c5891ec3232b4c23f396
deepak <dlipkumar77@gmail.com>
2025-04-27 20:18:00 +0530

    Add the update method in NoteDao

--- app/src/main/java/com/example/note_taking_app/data/local/NoteDao.kt
+++ app/src/main/java/com/example/note_taking_app/data/local/NoteDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.note_taking_app.model.Note
 
 @Dao
@@ -12,8 +13,14 @@ interface NoteDao {
     @Insert
     suspend fun insert(note: Note)
 
+    @Update
+    suspend fun update(note: Note)
+
     @Delete
     suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes WHERE id = :id LIMIT 1")
+    fun getNoteById(id: Long): LiveData<Note?>
 
     @Query("SELECT * FROM notes ORDER BY timestamp DESC")
     fun getAllNotes(): LiveData<List<Note>>
