048b1121700fb9aef0ff6ec3d37284a20a893448
deepak <dlipkumar77@gmail.com>
2025-04-27 17:10:09 +0530

    setup ViewModel

---
+++ app/src/main/java/com/example/note_taking_app/ui/main/NoteViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.note_taking_app.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.note_taking_app.data.repository.NoteRepository
+import com.example.note_taking_app.model.Note
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NoteViewModel @Inject constructor(private val repository: NoteRepository) : ViewModel() {
+
+    val allNotes: LiveData<List<Note>> = repository.allNotes
+
+    fun addNote(note: Note) {
+        viewModelScope.launch {
+            repository.insert(note)
+        }
+    }
+
+    fun deleteNote(note: Note) {
+        viewModelScope.launch {
+            repository.delete(note)
+        }
+    }
+}
