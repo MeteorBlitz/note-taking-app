08156e498528d3652ef61cbfe19464ad284c4a0b
deepak <dlipkumar77@gmail.com>
2025-04-27 17:07:51 +0530

    setup AppModule

---
+++ app/src/main/java/com/example/note_taking_app/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.example.note_taking_app.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.note_taking_app.data.local.NoteDao
+import com.example.note_taking_app.data.local.NoteDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(@ApplicationContext context: Context): NoteDatabase {
+        return Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            "note_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteDao(noteDatabase: NoteDatabase): NoteDao {
+        return noteDatabase.noteDao()
+    }
+}
